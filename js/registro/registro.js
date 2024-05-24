a5560ddf41696813deff8978f167170c6d7b595a
Lucas Chipolari <zlucaschipolariz@gmail.com>
2024-05-24 08:08:38 -0300

    cree los mensajes del registro e hice el footer

--- js/registro/registro.js
+++ js/registro/registro.js
@@ -3,11 +3,14 @@ import {
   validatePassword,
   validatePasswordConfirmation,
 } from "./registroValidaciones.js";
+import { Usuario } from "../admin/Usuario.js";
 
 const $form = document.getElementById("form");
 const $email = document.getElementById("email");
 const $password = document.getElementById("password");
 const $password2 = document.getElementById("password2");
+
+const usuarioPreCreado = new Usuario('admin@admin.com', 'admin');
 
 $email.addEventListener("blur", () => {
   validateEmail($email);
@@ -29,22 +32,33 @@ export const validateInputs = () => {
   const passwordValue = $password.value;
   const password2Value = $password2.value;
 
+  const isEmailValid = validateEmail($email);
+  const isPasswordValid = validatePassword($password);
+  const isPasswordConfirmationValid = validatePasswordConfirmation($password, $password2);
 
-  validateEmail($email);
-  validatePassword($password);
-  validatePasswordConfirmation($password, $password2);
-
-  if ($email && $password && $password2) {
+  if (usuarioPreCreado.email === emailValue && isPasswordValid && isPasswordConfirmationValid) {
     swal.fire({
-        title: 'Exito',
-        text: mensaje,
-        icon: 'success',
-        showConfirmButton: true,
-        showCancelButton: false,
-        confirmButtonText: 'Tremen2',
-      });
-    return true;
+      title: 'Error',
+      text: "El usuario ya existe",
+      icon: 'error',
+      showConfirmButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Iniciar sesión',
+      cancelButtonText: 'Cancelar',
+    });
+    return false;
+  }
 
+  if (isEmailValid && isPasswordValid && isPasswordConfirmationValid ) {
+    swal.fire({
+      title: 'Exito',
+      text: "Has creado tu cuenta correctamente!!",
+      icon: 'success',
+      showConfirmButton: true,
+      showCancelButton: false,
+      confirmButtonText: 'Oka',
+    });
+    return true;
   }
 
   return false;
