89c59a916eae3522f0f6d734ec2cef87d6a61cab
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-05-30 22:35:27 -0300

    modificacion de experiencia de usuario

--- js/index/slidePeliculasSeries.js
+++ js/index/slidePeliculasSeries.js
@@ -6,44 +6,81 @@ export const cargarSlider = () => {
   $swiperWrapper.innerHTML = "";
 
   peliculasSeries.forEach((peliculaSerie) => {
-    if(peliculaSerie.estaPublicada === "Si"){
-      const $slideCards = document.createElement("div")
-      $slideCards.classList.add("swiper-slide")
+    if (peliculaSerie.estaPublicada === "Si") {
+      const $slideCards = document.createElement("div");
+      $slideCards.classList.add("swiper-slide");
 
-      const $card = document.createElement("div")
-      $card.classList.add("card")
+      const $card = document.createElement("div");
+      $card.classList.add("card");
 
-      const $imgCard = document.createElement("img")
+      const $imgCard = document.createElement("img");
       $imgCard.src = peliculaSerie.image;
       $imgCard.alt = peliculaSerie.title;
 
-      const $overlayCard = document.createElement("div")
-      $overlayCard.classList.add("overlay-card")
+      const $overlayCard = document.createElement("div");
+      $overlayCard.classList.add("overlay-card","card");
 
-      const $text = document.createElement("div")
-      $text.classList.add("text")
+      const $text = document.createElement("div");
+      $text.classList.add("text","card-body","d-flex","flex-column","justify-content-between");
 
-      const $title = document.createElement("h5")
-      $title.textContent = peliculaSerie.title
+      const $title = document.createElement("h5");
+      $title.classList.add("card-title")
+      $title.textContent = peliculaSerie.title;
 
-      const $description = document.createElement("p")
+      const $description = document.createElement("p");
+      $description.classList.add("card-text","mb-4","flex-grow-1")
       $description.textContent = peliculaSerie.description;
 
-      $text.appendChild($title)
-      $text.appendChild($description)
-      $overlayCard.appendChild($text)
-      $card.appendChild($imgCard)
-      $card.appendChild($overlayCard)
-      $slideCards.appendChild($card)
-      $swiperWrapper.appendChild($slideCards)
+      const $divBotonesCard = document.createElement("div")
+      $divBotonesCard.classList.add("d-flex","justify-content-start")
 
+      const $btnReproducirCard = document.createElement("a");
+      $btnReproducirCard.href = "#";
+      $btnReproducirCard.classList.add(
+        "text-decoration-none","botones-card","mx-1","fs-5"
+      );
+      const $iconoReproducirCard = document.createElement("i");
+      $iconoReproducirCard.classList.add("ri-play-large-line");
+      $btnReproducirCard.appendChild($iconoReproducirCard);
+
+      const $btnFavoritoCard = document.createElement("a");
+      $btnFavoritoCard.href = "#";
+      $btnFavoritoCard.classList.add(
+        "text-decoration-none","botones-card","mx-1","fs-5"
+      );
+      const $iconoFavoritoCard = document.createElement("i");
+      $iconoFavoritoCard.classList.add("ri-bookmark-line");
+      $btnFavoritoCard.appendChild($iconoFavoritoCard);
+
+      const $btnDetallesCard = document.createElement("a");
+      $btnDetallesCard.href = "#";
+      $btnDetallesCard.classList.add(
+        "text-decoration-none","botones-card","mx-1","fs-5"
+      );
+      const $iconoDetallesCard = document.createElement("i");
+      $iconoDetallesCard.classList.add("ri-add-line");
+      $btnDetallesCard.appendChild($iconoDetallesCard);
+
+      $divBotonesCard.appendChild($btnReproducirCard);
+      $divBotonesCard.appendChild($btnFavoritoCard);
+      $divBotonesCard.appendChild($btnDetallesCard);
+
+      $text.appendChild($title);
+      $text.appendChild($description);
+      $text.appendChild($divBotonesCard);
+
+      $overlayCard.appendChild($text);
+      $card.appendChild($imgCard);
+      $card.appendChild($overlayCard);
+      $slideCards.appendChild($card);
+      $swiperWrapper.appendChild($slideCards);
     }
   });
 };
 
 new Swiper(".swiper-cards", {
   // Optional parameters
-  slidesPerView: "auto",
+
   spaceBetween: 15,
 
   direction: "horizontal",
@@ -54,5 +91,23 @@ new Swiper(".swiper-cards", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+    1400: {
+      slidesPerView: 6,
+    },
   },
 });
